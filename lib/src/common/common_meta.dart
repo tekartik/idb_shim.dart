b57462e1cb6a0d4fb4e485fff61cd33cf7a53a33
Alexandre Roux Tekartik <alex@tekartik.com>
2014-12-17 11:19:35 +0100

    pub version 0.6.2

---
+++ lib/src/common/common_meta.dart
@@ -0,0 +1,316 @@
+library tekartik_iodb.idb_meta;
+
+import 'package:idb_shim/idb_client.dart';
+import 'package:collection/equality.dart';
+
+class IdbTransactionMeta {
+  String mode;
+  List<String> storeNames;
+  IdbTransactionMeta(this.storeNames, this.mode);
+
+  // ref counting
+  // start on 0
+  int refCount;
+}
+
+class IdbDatabaseMeta {
+  int version;
+
+  IdbDatabaseMeta([this.version]);
+
+  IdbTransactionMeta _versionChangeTransaction;
+  Set<IdbObjectStoreMeta> versionChangeStores; // store modified during onUpgradeNeeded
+  Map<String, IdbObjectStoreMeta> _stores = new Map();
+
+  IdbTransactionMeta get versionChangeTransaction => _versionChangeTransaction;
+
+  onUpgradeNeeded(action()) {
+
+    versionChangeStores = new Set();
+    _versionChangeTransaction = new IdbTransactionMeta(null, IDB_MODE_READ_WRITE);
+    var result = action();
+    _versionChangeTransaction = null;
+    versionChangeStores = null;
+    return result;
+  }
+  createObjectStore(IdbObjectStoreMeta store) {
+    if (versionChangeTransaction == null) {
+      throw new StateError("cannot create objectStore outside of a versionChangedEvent");
+    }
+    versionChangeStores.add(store);
+    addObjectStore(store);
+  }
+
+  bool _containsStore(String storeName) {
+    return _stores.keys.contains(storeName);
+  }
+
+  IdbTransactionMeta transaction(storeName_OR_storeNames, String mode) {
+    // Check store(s) exist
+    if (storeName_OR_storeNames is String) {
+      if (!_containsStore(storeName_OR_storeNames)) {
+        throw new DatabaseStoreNotFoundError();
+      }
+      return new IdbTransactionMeta([storeName_OR_storeNames], mode);
+    } else if (storeName_OR_storeNames is List) {
+      for (String storeName in storeName_OR_storeNames) {
+        if (!_containsStore(storeName)) {
+          throw new DatabaseStoreNotFoundError();
+        }
+      }
+      return new IdbTransactionMeta(storeName_OR_storeNames, mode);
+    } else {
+      // assume null - it will complain otherwise
+      return new IdbTransactionMeta(storeName_OR_storeNames, mode);
+    }
+
+  }
+
+  addObjectStore(IdbObjectStoreMeta store) {
+    _stores[store.name] = store;
+  }
+
+  Iterable<String> get objectStoreNames => _stores.keys;
+
+  IdbObjectStoreMeta getObjectStore(String name) {
+    return _stores[name];
+  }
+
+  Map<String, Object> toDebugMap() {
+    Map map = {
+      "stores": _stores,
+      "version": version
+    };
+    return map;
+  }
+
+  @override
+  String toString() {
+    return toDebugMap().toString();
+  }
+
+  @override
+  int get hashCode => version;
+
+  @override
+  operator ==(o) {
+    if (o is IdbDatabaseMeta) {
+      return version == o.version;
+    }
+    return false;
+  }
+}
+
+// meta data is loaded only once
+class IdbObjectStoreMeta {
+
+  //final IdbDatabaseMeta databaseMeta;
+  final String name;
+  final String keyPath;
+  final bool autoIncrement;
+
+  Iterable<IdbIndexMeta> get indecies => _indecies.values;
+
+  Map<String, IdbIndexMeta> _indecies = new Map();
+
+  Iterable<String> get indexNames => _indecies.keys;
+
+  IdbIndexMeta index(String name) {
+    IdbIndexMeta indexMeta = _indecies[name];
+    if (indexMeta == null) {
+      throw new ArgumentError("index $name not found");
+    }
+    return indexMeta;
+  }
+
+  createIndex(IdbDatabaseMeta databaseMeta, IdbIndexMeta index) {
+    if (databaseMeta.versionChangeTransaction == null) {
+      throw new StateError("cannot create index outside of a versionChangedEvent");
+    }
+    databaseMeta.versionChangeStores.add(this);
+    addIndex(index);
+  }
+
+  IdbObjectStoreMeta.fromObjectStore(ObjectStore objectStore)
+      : this(objectStore.name, objectStore.keyPath, objectStore.autoIncrement);
+
+  IdbObjectStoreMeta(this.name, this.keyPath, bool autoIncrement, [List<IdbIndexMeta> indecies]) : autoIncrement = (autoIncrement == true) {
+    if (indecies != null) {
+      indecies.forEach((IdbIndexMeta indexMeta) {
+        addIndex(indexMeta);
+      });
+    }
+  }
+
+  IdbObjectStoreMeta.fromMap(Map<String, Object> map) //
+      : this(//
+      map["name"], //
+      map["keyPath"], //
+      map["autoIncrement"], IdbIndexMeta.fromMapList(map["indecies"]));
+
+  
+  IdbObjectStoreMeta clone() {
+    return new IdbObjectStoreMeta(name, keyPath, autoIncrement);
+  }
+  
+  addIndex(IdbIndexMeta index) {
+    _indecies[index.name] = index;
+  }
+
+  Map toDebugMap() {
+    return toMap();
+  }
+
+  Map<String, Object> toMap() {
+    Map map = {
+      "name": name
+    };
+    if (keyPath != null) {
+      map["keyPath"] = keyPath;
+    }
+    if (autoIncrement) {
+      map["autoIncrement"] = autoIncrement;
+    }
+    if (indecies.isNotEmpty) {
+      List<Map> indecies = [];
+      this.indecies.forEach((IdbIndexMeta indexMeta) {
+        indecies.add(indexMeta.toMap());
+      });
+      map['indecies'] = indecies;
+    }
+    return map;
+  }
+
+  @override
+  String toString() {
+    return toDebugMap().toString();
+  }
+
+  @override
+  int get hashCode => toMap().hashCode;
+
+  @override
+  operator ==(o) {
+    if (o is IdbObjectStoreMeta) {
+      return const DeepCollectionEquality().equals(toMap(), o.toMap());
+    }
+    return false;
+  }
+
+}
+
+class IdbCursorMeta {
+  var key;
+  bool get ascending => _ascending;
+  final bool autoAdvance;
+
+  KeyRange range;
+  bool _ascending;
+
+  String get direction => _ascending ? IDB_DIRECTION_NEXT : IDB_DIRECTION_PREV;
+
+  IdbCursorMeta(this.key, this.range, String direction, bool autoAdvance) : autoAdvance = autoAdvance == true {
+    if (direction == null) {
+      direction = IDB_DIRECTION_NEXT;
+    }
+
+    switch (direction) {
+      case IDB_DIRECTION_PREV:
+        _ascending = false;
+        break;
+      case IDB_DIRECTION_NEXT:
+        _ascending = true;
+        break;
+      default:
+        throw new ArgumentError("direction '$direction' not supported");
+    }
+    if (key != null && range != null) {
+      throw new ArgumentError("both key '${key}' and range '${range}' are specified");
+    }
+  }
+
+  Map toDebugMap() {
+
+    Map map = {
+      "direction": direction
+    };
+    if (key != null) {
+      map["key"] = key;
+    }
+    if (range != null) {
+      map["range"] = range;
+    }
+    if (autoAdvance) {
+      map["autoAdvance"] = autoAdvance;
+    }
+    return map;
+  }
+
+  @override
+  String toString() {
+    return toDebugMap().toString();
+  }
+}
+
+class IdbIndexMeta {
+  final String name;
+  final String keyPath;
+  final bool unique;
+  final bool multiEntry;
+  IdbIndexMeta(this.name, this.keyPath, bool unique, bool multiEntry)
+      : multiEntry = (multiEntry == true),
+        unique = (unique == true);
+
+
+  static List<IdbIndexMeta> fromMapList(List<Map> list) {
+    if (list == null) {
+      return null;
+    }
+    var metas = [];
+    list.forEach((map) {
+      metas.add(new IdbIndexMeta.fromMap(map));
+    });
+    return metas;
+  }
+  
+  IdbIndexMeta.fromMap(Map<String, Object> map) //
+      : this(map["name"], //
+      map["keyPath"], //
+      map["unique"], //
+      map["multiEntry"]);
+
+  IdbIndexMeta.fromIndex(Index index) : this(index.name, index.keyPath, index.unique, index.multiEntry);
+
+  Map toDebugMap() {
+    return toMap();
+  }
+  Map<String, Object> toMap() {
+    Map map = {
+      "name": name,
+      "keyPath": keyPath
+    };
+    if (unique) {
+      map["unique"] = unique;
+    }
+    if (multiEntry) {
+      map["multiEntry"] = multiEntry;
+    }
+    return map;
+  }
+
+  @override
+  String toString() {
+    return toDebugMap().toString();
+  }
+
+  @override
+  int get hashCode => toMap().hashCode;
+
+  @override
+  operator ==(o) {
+    if (o is IdbIndexMeta) {
+      return const MapEquality().equals(toMap(), o.toMap());
+    }
+    return false;
+  }
+}
---
+++ test/common_meta_test.dart
@@ -0,0 +1,75 @@
+library idb_shim.common_meta_test;
+
+import 'package:idb_shim/src/common/common_meta.dart';
+import 'idb_test_common.dart';
+
+// auto-increment, no key path
+final IdbObjectStoreMeta idbSimpleObjectStoreMeta = new IdbObjectStoreMeta(STORE_NAME, null, true);
+
+final IdbObjectStoreMeta idbObjectStoreMeta1 = new IdbObjectStoreMeta("name", "my_key", true);
+final IdbObjectStoreMeta idbObjectStoreMeta1Same = new IdbObjectStoreMeta("name", "my_key", true);
+final IdbObjectStoreMeta idbObjectStoreMeta2 = new IdbObjectStoreMeta("name", "my_key", false);
+final IdbObjectStoreMeta idbObjectStoreMeta3 = new IdbObjectStoreMeta("name", null, true);
+final IdbObjectStoreMeta idbObjectStoreMeta4 = new IdbObjectStoreMeta("other_name", "my_key", true);
+final List<IdbObjectStoreMeta> idbObjectStoreMetas = [idbObjectStoreMeta1, idbObjectStoreMeta2, idbObjectStoreMeta3];
+
+IdbIndexMeta idbIndexMeta1 = new IdbIndexMeta("name", "my_key", true, true);
+IdbIndexMeta idbIndexMeta1Same = new IdbIndexMeta("name", "my_key", true, true);
+IdbIndexMeta idbIndexMeta2 = new IdbIndexMeta("name", "my_key", true, false);
+IdbIndexMeta idbIndexMeta3 = new IdbIndexMeta("name", "my_key", false, true);
+IdbIndexMeta idbIndexMeta4 = new IdbIndexMeta("name", "other_key", true, true);
+IdbIndexMeta idbIndexMeta5 = new IdbIndexMeta("other_name", "my_key", true, true);
+final List<IdbIndexMeta> idbIndexMetas = [idbIndexMeta1, idbIndexMeta2, idbIndexMeta3, idbIndexMeta4, idbIndexMeta5];
+void main() => defineTests();
+
+void defineTests() {
+
+  group('meta', () {
+    test('database', () {
+      IdbDatabaseMeta meta1 = new IdbDatabaseMeta(1);
+      IdbDatabaseMeta meta2 = new IdbDatabaseMeta(1);
+      expect(meta1, meta2);
+      IdbDatabaseMeta meta3 = new IdbDatabaseMeta(2);
+      expect(meta1, isNot(meta3));
+
+    });
+
+    test('store', () {
+      expect(idbObjectStoreMeta1, idbObjectStoreMeta1Same);
+      expect(idbObjectStoreMeta1, isNot(idbObjectStoreMeta2));
+      expect(idbObjectStoreMeta1, isNot(idbObjectStoreMeta3));
+      expect(idbObjectStoreMeta1, isNot(idbObjectStoreMeta4));
+    });
+
+    test('index', () {
+      expect(idbIndexMeta1, idbIndexMeta1Same);
+      expect(idbIndexMeta1, isNot(idbIndexMeta2));
+      expect(idbIndexMeta1, isNot(idbIndexMeta3));
+      expect(idbIndexMeta1, isNot(idbIndexMeta4));
+      expect(idbIndexMeta1, isNot(idbIndexMeta5));
+    });
+
+    test('store with index', () {
+      IdbObjectStoreMeta meta1 = idbSimpleObjectStoreMeta.clone();
+      meta1.addIndex(idbIndexMeta1);
+      IdbObjectStoreMeta meta2 = idbSimpleObjectStoreMeta.clone();
+      meta2.addIndex(idbIndexMeta1);
+      expect(meta1, meta2);
+      IdbObjectStoreMeta meta3 = idbSimpleObjectStoreMeta.clone();
+      meta2.addIndex(idbIndexMeta2);
+      expect(meta1, isNot(meta3));
+    });
+
+    testStoreRoundTrip(IdbObjectStoreMeta meta) {
+      Map map = meta.toMap();
+      IdbObjectStoreMeta newMeta = new IdbObjectStoreMeta.fromMap(map);
+      expect(newMeta, meta);
+    }
+    
+    test('store to/from map', () {
+      IdbObjectStoreMeta meta1 = idbSimpleObjectStoreMeta.clone();
+      meta1.addIndex(idbIndexMeta1);
+      testStoreRoundTrip(meta1);
+    });
+  });
+}
