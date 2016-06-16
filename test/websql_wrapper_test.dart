be5facb4bd688b0276188f011bcb3af8d82ac3d5
Alexandre Roux Tekartik <alex@tekartik.com>
2016-06-16 16:43:35 +0200

    test: don't perform websql tests if not supported

--- test/websql_wrapper_test.dart
+++ test/websql_wrapper_test.dart
@@ -5,45 +5,49 @@ import 'package:dev_test/test.dart';
 import 'package:idb_shim/src/websql/websql_wrapper.dart';
 
 main() {
-  group('wrapper', () {
-    //wrapped.sqlDatabaseFactory.o
-    test('open', () {
-      SqlDatabase db = sqlDatabaseFactory.openDatabase(
-          "com.tekartik.test", "1", "com.tekartik.test", 1024 * 1024);
-      expect(db, isNotNull);
-      //wrapped.SqlTransaction transaction = db.transaction();
-    });
-
-    test('transaction', () {
-      SqlDatabase db = sqlDatabaseFactory.openDatabase(
-          "com.tekartik.test", "1", "com.tekartik.test", 1024 * 1024);
-      return db.transaction().then((SqlTransaction transaction) {
-        transaction.execute("DROP TABLE IF EXISTS test");
-        return transaction.completed;
+  if (SqlDatabase.supported) {
+    group('wrapper', () {
+      //wrapped.sqlDatabaseFactory.o
+      test('open', () {
+        SqlDatabase db = sqlDatabaseFactory.openDatabase(
+            "com.tekartik.test", "1", "com.tekartik.test", 1024 * 1024);
+        expect(db, isNotNull);
+        //wrapped.SqlTransaction transaction = db.transaction();
       });
-    });
 
-    test('select 1', () {
-      SqlDatabase db = sqlDatabaseFactory.openDatabase(
-          "com.tekartik.test", "1", "com.tekartik.test", 1024 * 1024);
-      return db.transaction().then((SqlTransaction transaction) {
-        transaction.execute("SELECT 0 WHERE 0 = 1").then((rs) {
-          expect(rs.rows.length, 0);
+      test('transaction', () {
+        SqlDatabase db = sqlDatabaseFactory.openDatabase(
+            "com.tekartik.test", "1", "com.tekartik.test", 1024 * 1024);
+        return db.transaction().then((SqlTransaction transaction) {
+          transaction.execute("DROP TABLE IF EXISTS test");
           return transaction.completed;
         });
       });
-    });
 
-    test('transaction 2 actions', () {
-      SqlDatabase db = sqlDatabaseFactory.openDatabase(
-          "com.tekartik.test", "1", "com.tekartik.test", 1024 * 1024);
-      return db.transaction().then((SqlTransaction transaction) {
-        return transaction.execute("DROP TABLE IF EXISTS test").then((_) {
-          return transaction.execute("CREATE TABLE test (name TEXT)").then((_) {
+      test('select 1', () {
+        SqlDatabase db = sqlDatabaseFactory.openDatabase(
+            "com.tekartik.test", "1", "com.tekartik.test", 1024 * 1024);
+        return db.transaction().then((SqlTransaction transaction) {
+          transaction.execute("SELECT 0 WHERE 0 = 1").then((rs) {
+            expect(rs.rows.length, 0);
             return transaction.completed;
           });
         });
       });
+
+      test('transaction 2 actions', () {
+        SqlDatabase db = sqlDatabaseFactory.openDatabase(
+            "com.tekartik.test", "1", "com.tekartik.test", 1024 * 1024);
+        return db.transaction().then((SqlTransaction transaction) {
+          return transaction.execute("DROP TABLE IF EXISTS test").then((_) {
+            return transaction
+                .execute("CREATE TABLE test (name TEXT)")
+                .then((_) {
+              return transaction.completed;
+            });
+          });
+        });
+      });
     });
-  });
+  }
 }
