c18b74751e6fa8aab85c03360da7694ce04ea8e8
alex <alex@tekartik.com>
2020-11-21 15:16:25 +0100

    nnbd: allow travis/github actions

--- tool/run_ci.dart
+++ tool/run_ci.dart
@@ -1,12 +1,17 @@
 import 'package:dev_test/package.dart';
+import 'package:process_run/shell.dart';
+import 'package:pub_semver/pub_semver.dart';
 
 Future main() async {
-  for (var dir in [
-    'idb_shim',
-    'idb_test',
-    // last
-    '.',
-  ]) {
-    await packageRunCi(dir);
+  var nnbdEnabled = dartVersion > Version(2, 12, 0, pre: '0');
+  if (nnbdEnabled) {
+    for (var dir in [
+      'idb_shim',
+      'idb_test',
+      // last
+      // '.',
+    ]) {
+      await packageRunCi(dir);
+    }
   }
 }
--- tool/travis.dart
+++ tool/travis.dart
@@ -1,16 +1,20 @@
 import 'package:process_run/shell.dart';
+import 'package:pub_semver/pub_semver.dart';
 
 Future main() async {
-  var shell = Shell();
+  var nnbdEnabled = dartVersion > Version(2, 12, 0, pre: '0');
+  if (nnbdEnabled) {
+    var shell = Shell();
 
-  for (var dir in ['idb_shim', 'idb_test']) {
-    shell = shell.pushd(dir);
-    await shell.run('''
+    for (var dir in ['idb_shim', 'idb_test']) {
+      shell = shell.pushd(dir);
+      await shell.run('''
 
 pub get
 dart tool/travis.dart
 
     ''');
-    shell = shell.popd();
+      shell = shell.popd();
+    }
   }
 }
