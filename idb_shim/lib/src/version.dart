08178ea90f17308872cdff1b1b2b8e8e71f61f7a
Alexandre Roux <alex@tekartik.com>
2025-03-03 01:44:51 +0100

    remove version

--- idb_shim/lib/src/version.dart
+++
@@ -1,8 +0,0 @@
-// ignore: depend_on_referenced_packages
-import 'package:pub_semver/pub_semver.dart';
-
-/// Package version text
-const packageVersionText = '2.6.4';
-
-/// Package version
-final packageVersion = Version.parse(packageVersionText);
